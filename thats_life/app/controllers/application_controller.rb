8361e3182c3e10f8776beb0f6955add55e1461c3
mattbmoorin <tzaratape@gmail.com>
2021-01-06 01:56:01 -0600

    created tables

--- thats_life/app/controllers/application_controller.rb
+++ thats_life/app/controllers/application_controller.rb
@@ -5,7 +5,25 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+   # set :session_secret, ""
   end
+
+  # helpers do
+  #   def logged_in?
+  #     !!session[:user_id]
+  #   end
+
+  #   def current_user
+  #     User.find_by_id(session[:user_id])
+  #   end
+
+  #   def authenticate
+  #     if !logged_in?
+  #       redirect '/login'
+  #     end
+  #   end
+  # end
 
   get "/" do
     erb :welcome
