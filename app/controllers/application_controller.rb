7c2c1b1c777d5280f1011a8c669758a2f5b765dc
mattbmoorin <tzaratape@gmail.com>
2021-01-11 00:19:54 -0600

    removed some unecessary files, messing with css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "0xDe783jfu9eDSilr90e235REit6"
   end
 
+  get '/' do
+    erb :'/welcome'
+  end
+
   not_found do
     redirect "/posts"
   end
